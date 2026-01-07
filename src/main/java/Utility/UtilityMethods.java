e61c8d2b4a089940c8b06b1493c83ad658ec846f
Manish Sharma <manish22021996sharma@gmail.com>
2026-01-07 15:42:18 +0530

    committing first time

--- src/main/java/Utility/UtilityMethods.java
+++ src/main/java/Utility/UtilityMethods.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class UtilityMethods {
-
+    //  change made by master
     WebDriver driver;
     public WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
